b75068ed9f6f4ba5f08d69e0078ab13ee0ccbf22
hyh <hyhivan1994@gmail.com>
2022-11-04 21:06:48 -0700

    add upload and submit function

--- development/view.py
+++ development/view.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
 from markupsafe import escape
-import json
 import os
+import json
 import sys
 
 
-app = Flask(__name__)
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Submit")
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('/index.html')
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+    return render_template('/index.html', form=form)
+
+'''
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return render_template('/index.html')
+'''
 
 '''
 @app.route('/')
@@ -44,9 +71,9 @@ def not_found(e):
 
 # This starts the flask app configured to listen on port 900
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 900))
+    port = int(os.environ.get('PORT', 5500))
     app.run(debug=True, host='0.0.0.0', port=port)
 
 
 # curl -X GET http://localhost:800/api/post/42
-# docker stop $(docker ps -a -q) && docker image build -t flask_docker . && docker run -p 900:900 -d flask_docker
+# docker stop $(docker ps -a -q) && docker image build -t flask_docker . && docker run -p 5500:5500 -d flask_docker
